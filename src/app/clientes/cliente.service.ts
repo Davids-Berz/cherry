662d47eb0c5605753249a991df5edc9d00f63d83
David Saldivar <davids.berz@gmail.com>
2023-02-18 08:43:31 -0600

    trabajando con sweet alert

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Cliente } from './Cliente';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -11,8 +11,15 @@ export class ClienteService {
   constructor(private http: HttpClient) {}
 
   private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
 
   getClientes(): Observable<Cliente[]> {
     return this.http.get(this.urlEndPoint).pipe(map((res) => res as Cliente[]));
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {
+      headers: this.httpHeaders,
+    });
   }
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,5 +1,8 @@
+import { ClienteService } from './cliente.service';
 import { Cliente } from './Cliente';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -9,12 +12,16 @@ import { Component, OnInit } from '@angular/core';
 export class FormComponent implements OnInit {
   cliente: Cliente = new Cliente();
   title:string = 'Crear Cliente'
-  constructor() {}
+  constructor(private clienteService: ClienteService, private router: Router) {}
 
   ngOnInit(): void {}
 
   create(): void {
-    console.log('clicked!')
-    console.log(this.cliente)
+    this.clienteService.create(this.cliente).subscribe(
+      cliente =>  {
+        this.router.navigate(['/clientes'])
+        Swal.fire('Nuevo Cliente', `Cliente ${cliente.nombre} creado con exito`, 'success')
+      } 
+    )
   }
 }
