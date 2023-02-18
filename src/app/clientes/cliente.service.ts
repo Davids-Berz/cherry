9b1206100b774633ff82b9a98fedf8f1c7294810
David Saldivar <davids.berz@gmail.com>
2023-02-18 06:56:56 -0600

    format

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -4,20 +4,15 @@ import { Cliente } from './Cliente';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClienteService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  private urlEndPoint: string = 'http://localhost:8080/api/clientes'
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
 
   getClientes(): Observable<Cliente[]> {
-    return this.http.get(this.urlEndPoint).pipe(
-      map(res => res as Cliente[])
-    );
+    return this.http.get(this.urlEndPoint).pipe(map((res) => res as Cliente[]));
   }
-
 }
