6ce3674e8155a14cd462589aeb8a6deb060ad119
David Saldivar <davids.berz@gmail.com>
2023-02-20 17:19:47 -0600

    manejo de errores en editar

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,14 +1,16 @@
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { Cliente } from './Cliente';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import Swal, { SweetAlertIcon } from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClienteService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   private urlEndPoint: string = 'http://localhost:8080/api/clientes';
   private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
@@ -23,15 +25,31 @@ export class ClienteService {
     });
   }
 
-  getcliente(id: any):Observable<Cliente> {
-    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
+  getcliente(id: any): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError(e => {
+        this.router.navigate(['/clientes'])
+        this.infoSawlFire('Error al editar', e.error.mensaje, 'error');
+        return throwError(e);
+      })
+    );
   }
 
   update(cliente: Cliente): Observable<Cliente> {
-    return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders})
+    return this.http.put<Cliente>(
+      `${this.urlEndPoint}/${cliente.id}`,
+      cliente,
+      { headers: this.httpHeaders }
+    );
   }
 
-  delete(id : Number): Observable<Cliente> {
-    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers: this.httpHeaders})
+  delete(id: Number): Observable<Cliente> {
+    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {
+      headers: this.httpHeaders,
+    });
+  }
+
+  infoSawlFire(title: string, content: string, status?: SweetAlertIcon): void {
+    Swal.fire(title, content, status);
   }
 }
